9abb5226b784f11e0ea4c03be9a5072c887beb7e
JubaerHossain <jubaer01.cse@gmail.com>
2022-06-15 18:33:45 +0600

    s

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const MyGraphQLSchema = buildSchema(`
         product: Product!
         quantity: Int!
     }
-    
+
 `);
 
 const rootValue = {
@@ -65,8 +65,31 @@ const rootValue = {
             date: '2019-01-01',
             subtotal: 29.99,
             items: [
-                { product: { id: '1', title: 'The Catcher in the Rye', price: 9.99 }, quantity: 1 },
-                { product: { id: '2', title: 'The Hobbit', price: 9.99 }, quantity: 2 }
+                {
+                    product: { 
+                        id: '1', 
+                        title: 'The Catcher in the Rye', 
+                        price: 9.99,
+                        reviews: [
+                            { rating: 5, comment: 'I love this book' },
+                            { rating: 3, comment: 'It was ok' }
+                        ] 
+                    }, 
+                    quantity: 1 
+                },
+                { 
+                    product: 
+                    { 
+                        id: '2', 
+                        title: 'The Hobbit', 
+                        price: 9.99,
+                        reviews: [
+                            { rating: 5, comment: 'I love this book' },
+                            { rating: 3, comment: 'It was ok' }
+                        ] 
+                    }, 
+                    quantity: 2 
+                }
             ]
 
         }
