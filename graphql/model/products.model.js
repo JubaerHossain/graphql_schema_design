f903b37202b5e2251235968d06f6bdb3f47d8d40
JubaerHossain <jubaer01.cse@gmail.com>
2022-06-18 20:47:36 +0600

    filtering

--- graphql/model/products.model.js
+++ graphql/model/products.model.js
@@ -14,7 +14,7 @@ const products = [
     title: "The Hobbit",
     description:
       "A novel about a hobbit who is thrown into The Great Depression",
-    price: 9.99,
+    price: 6.99,
     reviews: [
       { rating: 5, comment: "I love this book" },
       { rating: 3, comment: "It was ok" },
@@ -26,6 +26,13 @@ function getAllProducts() {
   return products;
 }
 
+function productFilter(min, max) {
+  return products.filter(
+    (product) => product.price > min && product.price < max
+  );
+}
+
 module.exports = {
   getAllProducts,
+  productFilter,
 };
--- graphql/resolvers/products.resolvers.js
+++ graphql/resolvers/products.resolvers.js
@@ -1,8 +1,12 @@
-const productsModel = require('../model/products.model');
+const productsModel = require("../model/products.model");
 module.exports = {
   Query: {
     products: () => {
       const products = productsModel.getAllProducts();
+      return products;
+    },
+    productByPrice : (root, args) => {
+      const products = productsModel.productFilter(args.min, args.max);
       return products;
     },
   },
